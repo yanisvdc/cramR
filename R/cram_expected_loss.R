3d04131cd82a13a97a72c6d26a9bf0a92b07770f
yanis vandecasteele <yanisvdc.ensae@gmail.com>
2025-01-29 19:48:40 -0500

    simplify cram expected loss

--- R/cram_expected_loss.R
+++ R/cram_expected_loss.R
@@ -3,7 +3,7 @@
 #' This function computes the Cram expected loss estimator \eqn{\hat{R}_{\mathrm{cram}}}
 #' based on the given loss matrix and batch indices.
 #'
-#' @param loss A matrix of loss values with \eqn{N} rows (data points) and \eqn{K} columns (batches).
+#' @param loss A matrix of loss values with \eqn{N} rows (data points) and \eqn{K+1} columns (batches).
 #' We assume that the first column of the loss matrix contains only zeros.
 #' The following nb_batch columns contain the losses of each trained model for each individual.
 #' @param batch_indices A list where each element is a vector of indices corresponding to a batch.
@@ -15,9 +15,14 @@
 #' N <- 100  # Number of data points
 #' K <- 10   # Number of batches
 #'
-#' loss <- matrix(rnorm(N * K), nrow = N, ncol = K)  # Random loss values
-#' batch_indices <- split(1:N, rep(1:K, each = N / K))
+#' # Generate a loss matrix with K+1 columns, first column as zeros
+#' loss <- matrix(rnorm(N * (K+1)), nrow = N, ncol = K+1)
+#' loss[, 1] <- 0  # Ensure first column is zero
 #'
+#' # Create batch indices dynamically
+#' batch_indices <- split(1:N, rep(1:K, length.out = N))
+#'
+#' # Compute Cram Expected Loss
 #' cram_expected_loss(loss, batch_indices)
 #' @export
 
@@ -35,17 +40,21 @@ cram_expected_loss <- function(loss, batch_indices) {
 
   loss_diff <- pi[, 2:nb_batch] - pi[, 1:(nb_batch - 1)]
 
-  # Loop over batches
-  for (k in 1:(K - 1)) {
-    # Retrieve the batch indices for evaluation
-    current_batch <- unlist(batch_indices[(k + 1):K])  # Indices for \( i \geq kB + 1 \)
+  # Create the mask for batch indices
+  mask <- matrix(NA, nrow = nrow(loss_diff), ncol = ncol(loss_diff))
 
-    # Compute the cumulative loss difference for the batch
-    loss_diff <- sum(loss[current_batch, k + 1] - loss[current_batch, k])
-
-    # Update the Cram expected loss
-    R_cram <- R_cram + (1 / ((K - k) * B)) * loss_diff
+  for (k in 2:nb_batch) {
+    # Set to NA the rows corresponding to batches k and above for column k-1
+    mask[unlist(batch_indices[k:nb_batch]), k-1] <- 1
   }
 
-  return(R_cram)
+  # Apply the mask to loss_diff
+  loss_diff <- loss_diff * mask
+
+  # Calculate average for each column
+  column_averages <- apply(loss_diff, 2, function(x) mean(x, na.rm = TRUE))
+
+  cram_expected_loss <- sum(column_averages)
+
+  return(cram_expected_loss)
 }
